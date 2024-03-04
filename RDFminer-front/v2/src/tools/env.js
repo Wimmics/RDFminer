d8342da2fa55db49c7ae39e771625d6a7e59d2bc
RemiFELIN <remi.felin@gmail.com>
2024-03-04 23:39:34 +0100

    fix env front

--- RDFminer-front/v2/src/tools/env.js
+++ RDFminer-front/v2/src/tools/env.js
@@ -3,4 +3,4 @@ import io from "socket.io-client";
 const serverUrl = process.env.NODE_ENV == "production" ? "http://134.59.130.136" : "http://localhost:8890"; // + process.env.VUE_APP_RDFMINER_SERVER_PORT;
 
 export const base = serverUrl;
-export const socket = io(serverUrl);
+export const socket =  process.env.NODE_ENV == "production" ? io(serverUrl + "/ws") : io(serverUrl);
